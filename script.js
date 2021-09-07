7e9248e30c00cc71ada0cb0f532d387dd519eb44
Ereh Maryann <edwardmaryann09@gmail.com>
2021-09-07 14:02:22 -0700

    selected all the html elements needed and assigned them to a variable

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select all the Html elements needed
+// and store them in variables
+const form = document.querySelector('#form');
+const password1El = document.querySelector('#password1');
+const password2El = document.querySelector('#password2');
+const messageContainer = document.querySelector('.message');
+const messageEl = document.querySelector('#message');
